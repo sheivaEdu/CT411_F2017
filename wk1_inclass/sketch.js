4de00edfef621ee75f29bdee76f85c08f696f4fa
sheivaEdu <31420742+sheivaEdu@users.noreply.github.com>
2017-09-05 19:23:20 -0400

    initial upload, week one homework

---
+++ wk1_inclass/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+createCanvas(800, 600);    
+    
+    
+}
+function draw(){
+background(130);
+fill(0);
+noStroke();
+rect(100, 200, 100, 100);
+noFill();
+//fill(255, 13, 100, 0);
+stroke(255);
+strokeWeight(10);
+ellipse(100, 200, 100, 100);
+    
+}
