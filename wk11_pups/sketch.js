ed826087cc03208c2469bd64eb4c901ef76a1f52
sheivaEdu <31420742+sheivaEdu@users.noreply.github.com>
2017-11-15 23:30:39 -0500

    Added more dog names

--- wk11_pups/sketch.js
+++ wk11_pups/sketch.js
@@ -1,4 +1,4 @@
-var pups = ["Rex", "Dexter", "Patches", "Scruffy", "Scraggle"];
+var pups = ["Rex", "Dexter", "Patches", "Scruffy", "Scraggle", "Matcha", "Mr. McGoo", "taco", "Schnuggs", "Droopy", "Tinks", "Pamplimouse", "Dandy"];
 var index = 0;
 
 function setup() {
